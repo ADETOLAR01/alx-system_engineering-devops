45815e0c45f70f007e5f34a0b4fb911426b940bd
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2023-05-10 02:26:35 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import json
+"""function that queries the Reddit API"""
 import requests
-
+import sys
 
 def number_of_subscribers(subreddit):
-    user = {"User-Agent": "Ahmed_belhaj"}
+    user = {"User-Agent": "fake_user_agent"}
     request = requests.get("https://www.reddit.com/r/{}/about.json"
                            .format(subreddit), headers=user)
     if request.status_code == 200:
