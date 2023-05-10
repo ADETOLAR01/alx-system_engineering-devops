7e9c489702db25253c9942695b2522843dd4a122
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2023-05-10 02:43:04 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
-import json
+import sys
 import requests
 
-
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'this_is_a_fake_subreddit'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+        
+    data = response.json()
+    posts = data["data"]["children"]
+    
+    for post in posts:
+        print(post["data"]["title"])
