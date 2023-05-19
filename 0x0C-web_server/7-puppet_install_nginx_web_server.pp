14462a5c9f1b765ee7856b2fa44179d8aeb9a39e
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2023-05-19 21:12:41 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,31 @@
 # Install and config the nginx
-package { 'nginx':
-  ensure => installed,
-  name   => 'nginx',
+package {'nginx':
+  ensure => 'present',
 }
 
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-  path    => '/var/www/html/index.html'
+exec {'install':
+  command  => 'apt-get update ; apt-get -y install nginx',
+  provider => shell,
 }
 
-file_line { 'title':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  multiple => true
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | dd status=none of=/var/www/html/index.html',
+  provider => shell,
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec {'sed -i 's/listen\s*80;/listen 80 default_server;/g' /etc/nginx/sites-available/default,
 }
+
+echo 'location /redirect_me { return 301 http://example.com/new_page}' >> /etc/nginx/sites-available/default
+
+echo '<html>
+<head><title>404 Not Found</title></head>
+<body>
+    <h1>404 Not Found</h1>
+    <p>Ceci n&apos;est pas une page.</p>
+</body>
+</html>' > /var/www/html/404.html
+
+sed -i 's|#error_page 404 /404.html;|error_page 404 /404.html;|' /etc/nginx/sites-available/default
+
+service nginx restart4 page.
