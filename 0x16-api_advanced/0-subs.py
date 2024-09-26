1b21bb8af72c760732a4df7b2a847a2456ebf02c
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2024-09-26 10:29:33 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,16 +7,13 @@ URL = 'https://www.reddit.com'
 
 
 def number_of_subscribers(subreddit):
-    """Queries and returns the number of subscribers of a given subreddit"""
+    """Queries and returns the number of subcribers of a given subreddit"""
     try:
         endpoint = '/r/{}/about.json'.format(subreddit)
         headers = {"User-Agent": "Custom Agent"}
         res = requests.get(URL + endpoint,
                            headers=headers,
                            allow_redirects=False)
-        if res.status_code == 200:
-            return res.json().get('data', {}).get('subscribers', 0)
-        else:
-            return 0
+        return res.json().get('data').get('subscribers')
     except Exception:
         return 0
