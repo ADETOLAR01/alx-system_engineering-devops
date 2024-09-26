f3efebb924de642179d84871c03eef354210d6ef
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2024-09-26 12:52:00 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """ this module returns the number of subscribers """
-
-import requests
-import sys
-
+from requests import sys
 
 def number_of_subscribers(subreddit):
     """
     returns the numberof subscribers of a subreddit
     """
-    base = 'https://api.reddit.com/r/'
-    user_agent = {'User-agent': 'Mozilla/5.0'}
+    base = 'https://www.reddit.com/'
+    user_agent = {'user-agent': 'my-app/0.0.1'}
     r = requests.get(base + subreddit + '/about', headers=user_agent,
                      allow_redirects=False)
     if (r.status_code == 404):
