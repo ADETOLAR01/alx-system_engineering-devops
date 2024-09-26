8d0d8cd0f3d6f26b2d4dfcd52a96de4feccaf961
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2024-09-26 10:17:19 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+"""
+Module for requesting the Reddit API
+"""
 import requests
+URL = 'https://www.reddit.com'
+
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    
+    """Queries and returns the number of subcribers of a given subreddit"""
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
+        endpoint = '/r/{}/about.json'.format(subreddit)
+        headers = {"User-Agent": "Custom Agen"}
+        res = requests.get(URL+endpoint,
+                           headers=headers,
+                           allow_redirects=False)
+        return res.json().get('data').get('subscribers')
     except Exception:
         return 0
